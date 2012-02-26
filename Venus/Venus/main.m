a3bd9ea296fddf7019f3c7e20b62efd911a42d3e
David Wahlandt <warly@hotmail.de>
2012-02-26 14:08:19 +0100

    - first commit of venus game

---
+++ Venus/Venus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Venus
+//
+//  Created by David Wahlandt on 26.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAAppDelegate class]));
+	}
+}
